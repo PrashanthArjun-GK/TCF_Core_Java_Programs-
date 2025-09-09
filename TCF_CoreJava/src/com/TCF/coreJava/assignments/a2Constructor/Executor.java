3c0baa1c440c3861d1224bec43c476862f9d5bc2
PrashanthArjun-GK <programmergkpit@gmail.com>
2025-09-09 13:50:46 +0530

    Update Executor.java

--- TCF_CoreJava/src/com/TCF/coreJava/assignments/a2Constructor/Executor.java
+++ TCF_CoreJava/src/com/TCF/coreJava/assignments/a2Constructor/Executor.java
@@ -4,17 +4,15 @@ public class Executor {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1=new Student(1,"Pooja","JFS");
-		Student s2=new Student(2,"Navvin","JFS");
-		Student s3=new Student(3,"Fowzz","DESIGNER");
 		
+		Student s1=new Student(1,"Arjun","TCF");
+		Student s2=new Student(2,"Karthi","TCF");
+		Student s3=new Student(3,"Nandha","TCF");
 		
 		s1.displayInfo();
 		s2.displayInfo();
 		s3.displayInfo();
-
 		
-
 	}
 
 }
