1d2aa1ede6348b1c249e1fbd3a2eb52a15c61b3b
PrashanthArjun-GK <programmergkpit@gmail.com>
2025-09-09 13:48:14 +0530

    Add files via upload

---
+++ TCF_CoreJava/src/com/TCF/coreJava/assignments/a2Constructor/Executor.java
@@ -0,0 +1,20 @@
+package com.tcf.Assignment_2.constructor;
+
+public class Executor {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student(1,"Pooja","JFS");
+		Student s2=new Student(2,"Navvin","JFS");
+		Student s3=new Student(3,"Fowzz","DESIGNER");
+		
+		
+		s1.displayInfo();
+		s2.displayInfo();
+		s3.displayInfo();
+
+		
+
+	}
+
+}
